bd9d855a782fcc282332650fbc5b18aad9045404
Hamos93 <gksmfwm00@naver.com>
2021-05-10 16:48:06 +0900

    영속계층 CRUD 구현

---
+++ Board/src/main/java/org/sample/mapper/BoardMapper.java
@@ -0,0 +1,29 @@
+package org.sample.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.sample.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	/* [ Create ] */
+	// 1. insert만 처리되고 생성된 PK 값을 알 필요가 없는 경우
+	public void insert(BoardVO board);
+
+	// 2. insert문이 실행되고 생성된 PK 값을 알아야 하는 경우
+	public void insertSelectKey(BoardVO board);
+
+	/* [ Read ] */
+	// @Select("select * from TBL_BOARD")
+	public List<BoardVO> getList();	
+
+	public BoardVO read(Long bno);
+
+	/* [ Update ] */
+	public int update(BoardVO board);
+	
+	/* [ delete ] */
+	public int delete(Long bno);
+
+}
---
+++ Board/src/test/java/org/sample/mapper/BoardMapperTests.java
@@ -0,0 +1,76 @@
+package org.sample.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sample.domain.BoardVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	
+	// BoardMapperTests 클래스는 스프링을 이용해서 BoardMapper 인터페이스의 구현체를 주입받아서 동작
+	// setter 의존성 주입
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+	@Test
+	public void insert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("insert 테스트");
+		board.setContent("insert 테스트");
+		board.setWriter("insert 테스트");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void insertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("insertSelectKey 테스트");
+		board.setContent("insertSelectKey 테스트");
+		board.setWriter("insertSelectKey 테스트");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+
+	@Test
+	public void getList() {
+		mapper.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void read() {
+		BoardVO board = mapper.read(5L);
+		log.info(board);
+	}
+	
+	@Test
+	public void update() {
+		BoardVO board = mapper.read(5L);
+		
+		board.setTitle("update 테스트");
+		board.setContent("update 테스트");
+		board.setWriter("update 테스트");
+		
+		int count = mapper.update(board);
+		log.info("[ UPDATE COUNT ] : " + count);
+	}
+	
+	@Test
+	public void delete() {
+		int count = mapper.delete(1L);
+		log.info("[ DELETE COUNT ] : " + count);
+	}
+	
+}
