7328e509e88866b2019658ccebd5b7bc7da227b1
Hamos93 <gksmfwm00@naver.com>
2021-05-25 16:57:59 +0900

    [서버] 댓글 서비스계층 구현

---
+++ Board/src/main/java/org/sample/service/ReplyService.java
@@ -0,0 +1,23 @@
+package org.sample.service;
+
+import java.util.List;
+
+import org.sample.domain.BoardVO;
+import org.sample.domain.Criteria;
+import org.sample.domain.ReplyVO;
+
+/* 메서드 설계 시 메서드 이름은 현실적인 로직의 이름을 붙이는 것이 관례 */
+
+public interface ReplyService {
+
+	public int register(ReplyVO reply);
+	
+	public ReplyVO get(Long rno);
+	
+	public int modify(ReplyVO reply);
+	
+	public int remove(Long rno);
+	
+	public List<ReplyVO> getList(Criteria cri, Long bno);
+
+}
---
+++ Board/src/main/java/org/sample/service/ReplyServiceImpl.java
@@ -0,0 +1,56 @@
+package org.sample.service;
+
+import java.util.List;
+
+import org.sample.domain.Criteria;
+import org.sample.domain.ReplyVO;
+import org.sample.mapper.ReplyMapper;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Service // 비즈니스 영역을 담당하는 객체임을 표시
+@AllArgsConstructor
+@Log4j
+public class ReplyServiceImpl implements ReplyService {
+	
+	/* [ 의존성 주입 ] */
+	// ReplyServiceImpl 객체는 ReplyMapper가 없으면 일 수행 X
+	// 스프링 4.3 이상에서 단일 생성자의 묵시적 자동주입
+	private ReplyMapper mapper;
+
+	@Override
+	public int register(ReplyVO reply) {
+		log.info("[ Service ] " + reply);
+
+		return mapper.insert(reply);
+	}
+
+	@Override
+	public ReplyVO get(Long rno) {
+		log.info("[ Service ] " + rno + "번 댓글 조회");
+
+		return mapper.read(rno);
+	}
+
+	@Override
+	public int modify(ReplyVO reply) {
+		log.info("[ Service ] 댓글 수정");
+		
+		return mapper.update(reply);
+	}
+
+	@Override
+	public int remove(Long rno) {
+		log.info("[ Service ] " + rno + "번 댓글 삭제");
+		return mapper.delete(rno);
+	}
+
+	@Override
+	public List<ReplyVO> getList(Criteria cri, Long bno) {
+		log.info("[ Service ] " + bno + "번 댓글 목록");
+		return mapper.getListWithPaging(cri, bno);
+	}
+	
+}
