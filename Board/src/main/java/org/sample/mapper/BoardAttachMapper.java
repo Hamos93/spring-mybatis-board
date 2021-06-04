518c5034d84660521c0a35086d42f591db14b8bc
Hamos93 <gksmfwm00@naver.com>
2021-06-04 17:09:01 +0900

    BoardAttachVO 생성 및 Mapper 추가

---
+++ Board/src/main/java/org/sample/mapper/BoardAttachMapper.java
@@ -0,0 +1,14 @@
+package org.sample.mapper;
+
+import java.util.List;
+
+import org.sample.domain.BoardAttachVO;
+
+public interface BoardAttachMapper {
+
+	public void insert(BoardAttachVO vo);
+	
+	public void delete(String uuid);
+	
+	public List<BoardAttachVO> findByBno(Long bno);
+}
